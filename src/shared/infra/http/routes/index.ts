bb5577e91effc2920afd55c63b875058e568ab63
Wesley <wesleyfcarvalho@outlook.com>
2021-02-11 20:36:35 -0300

    feat: Add clerk route

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import providerRouter from '../../../../modules/Provider/routes/provider.routes';
 import userRouter from '../../../../modules/User/routes/user.routes';
 import sessionRouter from '../../../../modules/Sessions/routes/sessions.routes';
+import clerkRouter from '../../../../modules/Clerk/routes/clerk.routes';
 
 const routes = Router();
 
 routes.use('/provider', providerRouter);
 routes.use('/user', userRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/clerk', clerkRouter);
 
 export default routes;
