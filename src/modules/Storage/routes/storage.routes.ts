0136a235af3aced89c3abef251d439a7d122946d
Gian <giangr21@gmail.com>
2021-04-03 20:38:33 -0300

    fix: remove authenticated

--- src/modules/Storage/routes/storage.routes.ts
+++ src/modules/Storage/routes/storage.routes.ts
@@ -1,14 +1,13 @@
 import { Router } from 'express';
-import ensureAuthenticated from '@modules/Sessions/middlewares/ensureAuthenticated';
 import StorageController from '../controllers/StorageController';
 
 const storageRouter = Router();
 const storageController = new StorageController();
 
-storageRouter.get('/img/:nome', ensureAuthenticated, storageController.getFile);
+storageRouter.get('/img/:nome', storageController.getFile);
 storageRouter.get('/img/min/:nome', storageController.getFileMin);
-storageRouter.get('/base64/:nome', ensureAuthenticated, storageController.getBase64);
-storageRouter.get('/base64/min/:nome', ensureAuthenticated, storageController.getMinBase64);
+storageRouter.get('/base64/:nome', storageController.getBase64);
+storageRouter.get('/base64/min/:nome', storageController.getMinBase64);
 storageRouter.post('/img', storageController.uploadFile);
 
 export default storageRouter;
